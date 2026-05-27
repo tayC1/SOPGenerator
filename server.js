ba6d2b532a03f2e47da51e5cf1348f310fe3c6eb
Taylor Giba <tay.christesson@gmail.com>
2026-05-27 10:33:27 -0500

    Increase size limit for screenshot payloads

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({ origin: true, credentials: true }));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/icons', express.static(path.join(__dirname, 'Icons')));
 
